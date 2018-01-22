cd532d4a57c550d1782a8551b66f91eeef931eac
tjbeirao <tjbeirao@gmail.com>
2018-01-22 20:01:02 +0000

    gitignore configured

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,14 @@
-var request = require('request');
-
-console.log('Welcome to the GitHub Avatar Downloader!');
-
-
+// var token = require('.secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    getRepoContributors("jquery", "jquery", function(err, result) {
-        console.log("Errors:", err);
-        console.log("Result:", result);
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+
+    request(options, function(err, res, body) {
+        cb(err, body);
     });
 }
